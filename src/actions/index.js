4d039a1aaac468be247978bafcc23855f00dbc9c
anaclarabck <ackyotoku@gmail.com>
2021-07-06 10:58:33 -0300

    Exercise one

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,21 @@
 export const REQUEST_API = 'REQUEST_API';
 export const GET_PICTURE = 'GET_PICTURE';
+export const FAILED_REQUEST = 'FAILED_REQUEST';
 
 export const requestAPI = () => ({ type: REQUEST_API });
 
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
+export const failedRequest = (error) => ({ type: FAILED_REQUEST, payload: error });
+
 export function fetchAPI() {
-  // Desenvolva aqui o código da action assíncrona
+  return (dispatch) => {
+    dispatch(requestAPI());
+    return fetch('https://aws.random.cat/meow')
+      .then((resolved) => resolved.json()
+        .then(
+          (json) => dispatch(getPicture(json)),
+          (error) => dispatch(failedRequest(error))
+        ));
+  }
 }
